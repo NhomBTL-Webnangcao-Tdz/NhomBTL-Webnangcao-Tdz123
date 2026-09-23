a2668726a5edb53a220cabf62e09e2723229181e
MrLee <lehaison205@gmail.com>
2026-09-23 12:04:03 +0700

    tao entity payment

--- quan-ly-ktx/src/modules/payment/payment.entity.ts
+++ quan-ly-ktx/src/modules/payment/payment.entity.ts
@@ -1,4 +1,22 @@
-import { Module } from '@nestjs/common';
+import {Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, } from 'typeorm';
 
-@Module({})
-export class PaymentModule {}
+import { Contract } from '../contract/contract.entity';
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn()
+  payment_id: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount: number;
+
+  @Column({ type: 'datetime' })
+  payment_date: Date;
+
+  @Column({ length: 50 })
+  payment_type: string;
+
+  @ManyToOne(() => Contract, (contract) => contract.payments)
+  @JoinColumn({ name: 'contract_id' })
+  contract: Contract;
+}
