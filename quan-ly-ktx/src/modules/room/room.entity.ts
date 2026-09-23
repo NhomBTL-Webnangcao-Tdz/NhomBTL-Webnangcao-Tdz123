858aaeff0a98c620252eec45b59c6d47e89d55af
dnamdeptrai <dnam7979@gmail.com>
2026-09-23 11:03:57 +0700

    Tạo entity cho Room

--- quan-ly-ktx/src/modules/room/room.entity.ts
+++ quan-ly-ktx/src/modules/room/room.entity.ts
@@ -1,4 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
+@Entity()
+export class Room extends BaseEntity {
+    @Column({ name: 'room_number' })
+  roomNumber: string;
 
-@Module({})
-export class RoomModule {}
+  @Column()
+  building: string;
+
+  @Column({ name: 'max_capacity', type: 'int' })
+  maxCapacity: number;
+
+  @Column({ name: 'current_occupancy', type: 'int', default: 0 })
+  currentOccupancy: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+}
